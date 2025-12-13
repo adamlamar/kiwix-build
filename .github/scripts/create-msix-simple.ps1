056e926e90baebfd1d4469a32ad1f8544fb6d038
Adam Lamar <adamlamar@users.noreply.github.com>
2025-12-12 18:45:59 -0700

    fix path

--- .github/scripts/create-msix-simple.ps1
+++ .github/scripts/create-msix-simple.ps1
@@ -152,7 +152,7 @@ BrowserSubprocessPath = QtWebEngineProcess.exe
     }
 
     # Copy the manifest file
-    $ManifestSource = Join-Path "windows" "msix" "Package.appxmanifest"
+    $ManifestSource = Join-Path -Path "windows" -ChildPath "msix" | Join-Path -ChildPath "Package.appxmanifest"
     $ManifestDest = Join-Path $StagingDir "AppxManifest.xml"
 
     if (-not (Test-Path $ManifestSource)) {
